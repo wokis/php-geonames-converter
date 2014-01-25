650286be8c5317f67b49b7765a028ed1e5fa5fdd
Kacper <kacper@kacper.se>
2014-01-25 17:09:17 +0100

    Refined example file and added sample code for AllCountriesSource.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-// Countries from http://download.geonames.org/export/dump/countryInfo.txt
+// CSV files from http://download.geonames.org/export/dump/
 
-//ini_set('memory_limit', '150M');
+ini_set('max_execution_time', 0);
 
 use Wokis\Geonames\CountryInfoSource;
-use Wokis\Geonames\BigCitiesSource;
+use Wokis\Geonames\AllCountriesSource;
 use Wokis\Geonames\JsonConverter;
 
 include_once 'vendor/autoload.php';
@@ -16,8 +16,20 @@ $converter = new JsonConverter();
 $converter->source(new CountryInfoSource('countryInfo.txt'));
 $converter->convert('dist/countries.json');
 
+/** 
+ * allCountries.txt is a very large file. Converting it will take a lot of time,
+ *  therefore it's commented out
+ */
+//$converter = new JsonConverter();
+//$converter->source(new AllCountriesSource('allCountries.txt'));
+//$converter->convert('dist/allCountries.json');
+
 $endTime = microtime(true);
 
 $execTime = ($endTime - $startTime);
 
-echo sprintf('All requested operations completed in <i>%s</i> seconds.', $execTime);
+$hours = floor($execTime / 3600);
+$minutes = floor(($execTime - ($hours*3600)) / 60);
+$seconds = floor($execTime % 60);
+
+echo sprintf('All requested operations completed in <i>%s</i> hour(s), <i>%s</i> minute(s) and <i>%s</i> second(s).', $hours, $minutes, $seconds);
